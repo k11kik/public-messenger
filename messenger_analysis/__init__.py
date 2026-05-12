538c6f27c0903edfb1acf346ed406efe5a60ebc4
k11kik <143200648+k11kik@users.noreply.github.com>
2026-05-12 17:43:04 +0900

    Update via script

---
+++ messenger_analysis/__init__.py
@@ -0,0 +1,15 @@
+"""
+messenger_analysis
+
+* messenger_analysis.py: run by main file (MessengerAnalysis.py)
+* analysis: Champion file (with pytplot)
+* other modules: without pytplot
+"""
+
+from . import (
+    getdata,
+    analysis,
+    event_search
+)
+
+# __all__ = ['getdata']
